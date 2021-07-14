136d5e287c0988c8f92ade34922bca5ce61279cb
Ibrahim Jarada <ibrahim.jarada@gmail.com>
2021-07-14 23:35:19 +0300

    a try with add function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import './App.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import {useState} from 'react'
+import {render} from 'react-dom'
+let list=[1];
+const input=document.getElementById('input');
+
+let btnFunction='new';
+let clickAction={
+  'edit':function(e){
+    e.preventDefault();
+    console.log(e.target.attributes.index.value)
+  },
+'delete':function(e){
+  e.preventDefault();
+  console.log(e.target.attributes.index.value)
+
+}
+}
+
+function Itemslist (props){
+  let itemsarray=props.list
+  if(itemsarray.length>0){
+  const items=itemsarray.map((item,index)=>(<li class="list-group-item d-flex justify-content-between align-items-center" >
+  <span class="title">{item}</span>
+  <span>
+      <a href="#" onClick={props.onClick.edit} data-edit ><i class="bi bi-pencil-square blue" index={index} ></i></a>
+      <a href="#" onClick={props.onClick.delete}  data-delete><i class="bi bi-x-circle red" index={index}></i></a>
+  </span>
+</li>))
+return items
+  }
+  return ""
+}
+
+
+
 
 function App() {
+  console.log("render")
+  var [itemsarray,setItemsarray]=useState(list);
+  let value;
+  const handleChange=(e)=>{
+    e.preventDefault();
+value=e.target.value;
+console.log(value,1);
+  }
+const add=(e)=>{
+  console.log(value,1,2,3)
+  itemsarray.push(value);
+ setItemsarray(itemsarray);
+//  render(<App />,document.getElementById('root'))
+
+
+}
   return (
     <div className="App">
     <div className="add-task">
-              <input type="text" class="todo-input" placeholder="        Add task..."/>
-              <button class="todo-button" type="submit">+</button>
+              <input type="text" class="todo-input" id="input" placeholder="        Add task..." onChange={handleChange}/>
+              <button class="todo-button" type="submit" onClick={add}>+</button>
        </div>
        <section class="todo-container">
         <ul class="todo-list">
-        <li>meeting
-          <button className="deleteButton">delete<i class="far fa-check" ></i> </button>
-          <button className="deleteButton">done<i class="fas fa-trash"></i> </button>
-        </li>
-        <li>meeting
-        <button className="deleteButton">delete<i class="far fa-check" ></i> </button>
-          <button className="deleteButton">done<i class="fas fa-trash"></i> </button>
-          </li>
-
+        <Itemslist list={itemsarray} onClick={clickAction}/>
         </ul>
     </section>
   
@@ -25,5 +69,4 @@ function App() {
   
   );
 }
-
 export default App
