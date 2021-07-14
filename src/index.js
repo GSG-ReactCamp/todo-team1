83d3064cbc9fb633ded10ea0e36e5a6a90f8d710
Ibrahim Jarada <ibrahim.jarada@gmail.com>
2021-07-14 23:39:39 +0300

    state hooks proplem

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM,{render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
