f234dfb473563a9cc0cb32aa4af9f4a7ca624a4d
Ibrahim Jarada <ibrahim.jarada@gmail.com>
2021-07-15 23:40:45 +0300

    Applying requested changes

--- src/App.js
+++ src/App.js
@@ -1,69 +1,78 @@
+// eslint-disable-next-line linebreak-style
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable linebreak-style */
+import { useState, useMemo } from 'react';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import './App.css';
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
-import {useState} from 'react'
-import {render} from 'react-dom'
-let list=[];
-const input=document.getElementById('input');
 
-let btnFunction='new';
-let clickAction={
-  'edit':function(e){
+// let btnFunction='new'; plan to use it later
+const clickAction = {
+  edit(e) {
     e.preventDefault();
-    console.log(e.target.attributes.index.value)
+    // e.target.attributes.index.value ; plan to use it later
   },
-'delete':function(e){
-  e.preventDefault();
-  console.log(e.target.attributes.index.value)
+  delete(e) {
+    e.preventDefault();
+    // e.target.attributes.index.value ; plan to use it later
+  },
+};
 
+function Itemslist(props) {
+  const itemsarray = { ...props }.list;
+  const items = useMemo(
+    () => itemsarray.map((item, index) => (
+      <li
+        className="list-group-item d-flex justify-content-between align-items-center"
+        key={index}
+      >
+        <span className="title">{item}</span>
+        <span>
+          <a href="#" onClick={props.onClick.edit} data-edit>
+            <i className="bi bi-pencil-square blue" index={index} />
+          </a>
+          <a href="#" onClick={props.onClick.delete} data-delete>
+            <i className="bi bi-x-circle red" index={index} />
+          </a>
+        </span>
+      </li>
+    )),
+    [itemsarray],
+  );
+  return <ul {...props}>{items}</ul>;
 }
-}
-
-function Itemslist (props){
-  let itemsarray=props.list
-  if(itemsarray.length>0){
-  const items=itemsarray.map((item,index)=>(<li className="list-group-item d-flex justify-content-between align-items-center" >
-  <span className="title">{item}</span>
-  <span>
-      <a href="#" onClick={props.onClick.edit} data-edit ><i className="bi bi-pencil-square blue" index={index} ></i></a>
-      <a href="#" onClick={props.onClick.delete}  data-delete><i className="bi bi-x-circle red" index={index}></i></a>
-  </span>
-</li>))
-return items
-  }
-  return ""
-}
-
-
-
 
 function App() {
-  console.log("render")
-  var [itemsarray,setItemsarray]=useState(list);
-  let value;
-  const handleChange=(e)=>{
-    e.preventDefault();
-value=e.target.value;
-console.log(value,1);
-  }
-const add=(e)=>{
-let array=[...itemsarray];
-  array.push(value);
- setItemsarray(array);
-}
+  const [itemsarray, setItemsarray] = useState([]);
+  const [inputvalue, setInputvalue] = useState('');
+  const handleChange = (e) => {
+    setInputvalue(e.target.value);
+  };
+  const handleSubmit = () => {
+    itemsarray.push(inputvalue);
+    setItemsarray([...itemsarray]);
+  };
   return (
     <div className="App">
-    <div className="add-task">
-              <input type="text" className="todo-input" id="input" placeholder="        Add task..." onChange={handleChange}/>
-              <button className="todo-button" type="submit" onClick={add}>+</button>
-       </div>
-       <section className="todo-container">
-        <ul className="todo-list">
-        <Itemslist list={itemsarray} onClick={clickAction}/>
-        </ul>
-    </section>
-  
-</div>
-  
+      <div className="add-task">
+        <input
+          type="text"
+          className="todo-input"
+          id="input"
+          placeholder="Add task..."
+          onChange={handleChange}
+        />
+        <button className="todo-button" type="submit" onClick={handleSubmit}>
+          +
+        </button>
+      </div>
+      <section className="todo-container">
+        <Itemslist
+          className="todo-list"
+          list={itemsarray}
+          onClick={clickAction}
+        />
+      </section>
+    </div>
   );
 }
-export default App
+export default App;
