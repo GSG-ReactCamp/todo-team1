8e62d25b3a5aaa98b98fc8724dd614d191648a01
Ibrahim Jarada <ibrahim.jarada@gmail.com>
2021-07-17 20:43:59 +0300

    implementing edit feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,20 @@ import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 
 function Itemslist(props) {
-  const { className, list: itemsArray, handelDelete } = props;
+  const {
+    className, list: itemsArray, handelDelete, handelEdit,
+  } = props;
   const items = useMemo(
-    () => itemsArray.map((item) => (
+    () => itemsArray.map((item, index) => (
       <li
         className="list-group-item d-flex justify-content-between align-items-center"
         key={item.id}
       >
         <span className="title">{item.task}</span>
         <span>
+          <button type="button" onClick={() => handelEdit(index)} data-edit>
+            <i className="bi bi-pencil-square blue" />
+          </button>
           <button type="button" onClick={() => handelDelete(item.id)} data-delete>
             <i className="bi bi-x-circle red" index={item.id} />
           </button>
@@ -30,21 +35,37 @@ Itemslist.propTypes = {
   className: PropTypes.string.isRequired,
   list: PropTypes.arrayOf(PropTypes.string).isRequired,
   handelDelete: PropTypes.func.isRequired,
+  handelEdit: PropTypes.func.isRequired,
 };
 
 function App() {
   const [itemsArray, setitemsArray] = useState([]);
   const [inputvalue, setInputvalue] = useState('');
+  const [btnFunction, setBtnFunction] = useState('newItem');
+  const [editindex, setEditindex] = useState(-1);
+
+  const handelEdit = (index) => {
+    setEditindex(index);
+    setBtnFunction('edit');
+  };
 
   const handelDelete = (id) => {
     const newList = itemsArray.filter((item) => item.id !== id);
     setitemsArray(newList);
+    setBtnFunction('newItem'); // in order if the user press edit then delete without submiting new value
   };
   const handleChange = (e) => {
     setInputvalue(e.target.value);
   };
-  const handleSubmit = () => {
-    setitemsArray([...itemsArray, { id: uuidv4(), task: inputvalue }]);
+  const handleSubmit = {
+    newItem() { setitemsArray([...itemsArray, { id: uuidv4(), task: inputvalue }]); },
+    edit() {
+      const newArray = [...itemsArray];
+      newArray[editindex].task = inputvalue;
+      setitemsArray(newArray);
+      setBtnFunction('newItem');
+    },
+
   };
   return (
     <div className="App">
@@ -56,7 +77,7 @@ function App() {
           placeholder="Add task..."
           onChange={handleChange}
         />
-        <button className="todo-button" type="submit" onClick={handleSubmit}>
+        <button className="todo-button" type="submit" onClick={handleSubmit[btnFunction]}>
           +
         </button>
       </div>
@@ -65,6 +86,7 @@ function App() {
           className="todo-list"
           list={itemsArray}
           handelDelete={handelDelete}
+          handelEdit={handelEdit}
         />
       </section>
     </div>
