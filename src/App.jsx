ee1ed8df3b271f22735e6bcb2a87c96b657fc516
Ibrahim Jarada <ibrahim.jarada@gmail.com>
2021-07-18 08:11:34 +0300

    unnecessary procedure in handelDone function

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,6 @@ function App() {
     const newArray = [...itemsArray];
     newArray[index].isDone = !newArray[index].isDone;
     setItemsArray(newArray);
-    setBtnFunction('newItem'); // in order if the user press edit then done without submiting new value
   };
 
   const handelEdit = (index) => {
